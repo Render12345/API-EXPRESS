8edf78ac92646bc8d5ffc379f93f1a26ced6021f
Jorge Luis Perez Manriquez <manriquezjorge100@gmail.com>
2026-03-23 21:02:20 -0600

    fix: export app for vercel

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,5 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
-const supabaseUrl = 'https://kcrrxwgehuukkwshyypp.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjcnJ4d2dlaHV1a2t3c2h5eXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzA0OTcsImV4cCI6MjA4OTYwNjQ5N30.FzqUbd4cOltKzva1jZN8N0S5ajkq52KprKPoIau7DY8';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = supabase;
